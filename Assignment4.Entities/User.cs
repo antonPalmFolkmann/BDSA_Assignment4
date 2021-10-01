ea849cc3909b8526c69182171329e6cc5d505c57
aHjelmgaard <90194013+aHjelmgaard@users.noreply.github.com>
2021-10-01 13:55:47 +0200

    Initial setup (#1)
    Co-authored-by: Alexander Hjelmgaard <hjel@itu.dk>

--- Assignment4.Entities/User.cs
+++ Assignment4.Entities/User.cs
@@ -1,6 +1,21 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment4.Entities
 {
     public class User
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public ICollection<Task> Tasks { get; set; }
     }
 }
