c5897daefd50136405fd1eea377572a752ec8085
Alexander Hjelmgaard <hjel@itu.dk>
2021-10-01 13:54:03 +0200

    Initial setup

--- Assignment4.Entities/KanbanContext.cs
+++ Assignment4.Entities/KanbanContext.cs
@@ -1,6 +1,14 @@
+using System;
+using Assignment4.Core;
+using System.Data;
+
+
 namespace Assignment4.Entities
 {
     public class KanbanContext
     {
+        //public DbSet<Task> Tasks { get; set; }
+        //public DbSet<Tag> Tags { get; set; }
+        //public DbSet<User> Users { get; set; }
     }
 }
--- Assignment4.Entities/Tag.cs
+++ Assignment4.Entities/Tag.cs
@@ -1,6 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Assignment4.Entities
 {
     public class Tag
     {
+        public int Id { get; set; }
+
+[Required]
+[StringLength(50)]
+        public string Name { get; set; }
     }
+
+    
 }
