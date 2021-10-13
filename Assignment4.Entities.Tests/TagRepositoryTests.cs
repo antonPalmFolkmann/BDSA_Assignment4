22fcd20252c9263ece01636e0f77345e1e75c5d7
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2021-10-13 11:59:25 +0200

    Feature/tagrepocontinued (#9)
    * Added tag repo tests
    
    * Finished implementing and testing tag repo

--- Assignment4.Entities.Tests/TagRepositoryTests.cs
+++ Assignment4.Entities.Tests/TagRepositoryTests.cs
@@ -3,44 +3,205 @@ using Xunit;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Assignment4.Core;
+using System.Collections.Generic;
 
 namespace Assignment4.Entities.Tests
 {
     public class TagRepositoryTests
     {
+        private readonly KanbanContext _context;
+        private readonly TagRepository _repository;
+        public TagRepositoryTests(){
+            var connection = new SqliteConnection("filename=:memory:");
+            connection.Open();
+            var builder = new DbContextOptionsBuilder<KanbanContext>();
+            builder.UseSqlite(connection);
+            var context = new KanbanContext(builder.Options);
+            context.Database.EnsureCreated();
+
+            var tag1 = new Tag {Id = 1, Name = "Tag1", Tasks = new List<Task>()};
+            var tag2 = new Tag {Id = 2, Name = "Tag2", Tasks = new List<Task>()};
+            var tag3 = new Tag {Id = 3, Name = "Tag3", Tasks = new List<Task>()};
+            var tags = new List<Tag>();
+
+            var task1 = new Task { Id = 1, Title = "Task 1", AssignedTo = null, Description = "Task 1 description", State = Core.State.Active, Tags = tags};
+            tag2.Tasks.Add(task1);
+            var tasks = new List<Task>();
+            tasks.Add(task1);
+
+            var user1 = new User { Id = 1, Name = "Name 1", Email = "title1@mail.com", Tasks = tasks};
+            task1.AssignedTo = user1;
+
+            var user2 = new User { Id = 2, Name = "Name 2", Email = "title2@mail.com", Tasks = new List<Task>()};
+
+            context.Add(tag1);
+            context.Add(tag2);
+            context.Add(tag3);
+
+            context.SaveChanges();
+
+            _context = context;
+            _repository = new TagRepository(_context);
+        }
+
         [Fact]
         public void Create_given_Tag_returns_response_and_Id()
         {
         //Given
-        using var connection = new SqliteConnection("filename=:memory:");
-        connection.Open();
-        var builder = new DbContextOptionsBuilder<KanbanContext>();
-        builder.UseSqlite(connection);
-        using var context = new KanbanContext(builder.Options);
-        context.Database.EnsureCreated();
-        
-        //When
-        var repo = new TagRepository(context);
         var tag = new TagCreateDTO{
             Name = "CreateTag"
         };
-        var created = repo.Create(tag);
-        var response = Response.Created;
+        
+        //When
+        var created = _repository.Create(tag);
         
         //Then
-        var tagDTO = new TagDTO(1, "CreateTag");
-        Assert.Equal(1, created.TagId);
-        Assert.Equal(response, created.Response);
+        Assert.Equal(4, created.TagId);
+        Assert.Equal(Response.Created, created.Response);
         }
 
         [Fact]
-        public void newTesttoBeImplemented()
+        public void Create_given_Tag_already_existing_should_return_Conflict()
         {
         //Given
+        var tag = new TagCreateDTO{
+            Name = "Tag1"
+        };
         
+        //When
+        var created = _repository.Create(tag);
+
+        //Then
+        Assert.Equal(Response.Conflict, created.Response);
+        }
+
+        [Fact]
+        public void Update_given_not_existing_id_returns_notfound()
+        {
+        //Given
+        var repo = new TagRepository(_context);
+        
+        //When
+        var tag = new TagUpdateDTO{
+            Id = 99
+        };
+
+        var updated = repo.Update(tag);
+        
+        //Then
+        Assert.Equal(Response.NotFound, updated);
+        }
+
+        [Fact]
+        public void Update_given_existing_id_returns_Updated()
+        {
+        //Given
+        var repo = new TagRepository(_context);
+        
+        //When
+        var tag = new TagUpdateDTO{
+            Id = 3,
+            Name = "tag3new"
+        };
+
+        var updated = repo.Update(tag);
+        
+        //Then
+        Assert.Equal(Response.Updated, updated);
+        }
+
+        [Fact]
+        public void Delete_given_existing_tagid_returns_deleted()
+        {
+        //Given
+        var repo = new TagRepository(_context);
+        
+        //When
+        var deleted = repo.Delete(1);
+        
+        //Then
+        Assert.Equal(Response.Deleted, deleted);
+        Assert.Null(_context.Tags.Find(1));
+        }
+
+        [Fact]
+        public void Delete_tag_already_in_use_without_force_returns_conflict()
+        {
+        //Given
+        var repo = new TagRepository(_context);
+        
+        //When
+        var deleted = repo.Delete(2);
+        
+        //Then
+        Assert.Equal(Response.Conflict, deleted);
+        }
+
+        [Fact]
+        public void Delete_tag_already_in_use_with_force_returns_deleted()
+        {
+        //Given
+        var repo = new TagRepository(_context);
+
+        //When
+        var deleted = repo.Delete(2,true);
+
+        //Then
+        Assert.Equal(Response.Deleted, deleted);
+        }
+
+        [Fact]
+        public void Delete_given_not_existing_id_returns_notfound()
+        {
+        //Given
+        var repo = new TagRepository(_context);
+        
+        //When
+        var deleted = repo.Delete(99);
+        
+        //Then
+        Assert.Equal(Response.NotFound, deleted);
+        }
+
+        [Fact]
+        public void Read_given_existing_id_returns_tag()
+        {
+        //Given
+        var tag = _repository.Read(3);
+        
+        //Then
+        Assert.Equal("Tag3", tag.Name);
+        Assert.Equal(3, tag.Id);
+        }
+
+        [Fact]
+        public void Read_given_not_existing_Id_returns_Null()
+        {
+        //Given
+        var tag = _repository.Read(99);
+        
+        //Then
+        Assert.Null(tag);
+        }
+
+        [Fact]
+        public void ReadAll_returns_all_tags()
+        {
+        //Given
+        var allTags = _repository.ReadAll();
+
         //When
         
         //Then
+        Assert.Collection(allTags,
+                t => Assert.Equal(new TagDTO(1, "Tag1"), t),
+                t => Assert.Equal(new TagDTO(2, "Tag2"), t),
+                t => Assert.Equal(new TagDTO(3, "Tag3"), t)
+            );
+        }
+
+        public void Dispose(){
+            _context.Dispose();
         }
     }
 }
--- Assignment4.Entities/TagRepository.cs
+++ Assignment4.Entities/TagRepository.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using Assignment4.Core;
+using System.Linq;
+using System;
 
 namespace Assignment4.Entities
 {
@@ -14,32 +16,77 @@ namespace Assignment4.Entities
 
         public (Response Response, int TagId) Create(TagCreateDTO tag)
         {
-            return (Response.Created, 1);
+            var tagsWithSameName = from c in _context.Tags
+                                    where c.Name == tag.Name
+                                    select new TagDTO(
+                                        c.Id,
+                                        c.Name
+                                    );
+            
+
+            if (tagsWithSameName.Count() != 0){
+                return (Response.Conflict, -1);
+            } 
+
+            var entity = new Tag{Name = tag.Name};
+
+            _context.Tags.Add(entity);
+
+            _context.SaveChanges();
+
+            return (Response.Created, entity.Id);
         }
 
         public Response Delete(int tagId, bool force = false)
         {
-            throw new System.NotImplementedException();
+            var entity = _context.Tags.Find(tagId);
+
+            if(entity == null){
+                return Response.NotFound;
+            }
+
+            if(entity.Tasks.Count != 0 && force == false){
+                return Response.Conflict;
+            }
+
+            _context.Tags.Remove(entity);
+            _context.SaveChanges();
+
+            return Response.Deleted;
         }
 
         public TagDTO Read(int tagId)
         {
-            throw new System.NotImplementedException();
+            var tags = from t in _context.Tags
+                        where t.Id == tagId
+                        select new TagDTO(
+                            t.Id,
+                            t.Name
+                        );
+            return tags.FirstOrDefault();
         }
 
-        public IReadOnlyCollection<TagDTO> ReadAll()
-        {
-            throw new System.NotImplementedException();
-        }
+        public IReadOnlyCollection<TagDTO> ReadAll() => 
+            _context.Tags
+            .Select(t => new TagDTO(t.Id, t.Name))
+            .ToList().AsReadOnly();
 
         public Response Update(TagUpdateDTO tag)
         {
-            throw new System.NotImplementedException();
+            var entity = _context.Tags.Find(tag.Id);
+            if(entity == null){
+                return Response.NotFound;
+            }
+
+            entity.Name = tag.Name;
+            _context.SaveChanges();
+
+            return Response.Updated;
         }
 
         public void Dispose()
         {
-            throw new System.NotImplementedException();
+            _context.Dispose();
         }
     }
 }
